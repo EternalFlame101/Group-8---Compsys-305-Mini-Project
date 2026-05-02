ba8a70c5e8aa37f5f49c44a88d0d3c640b3137b9
Johnny Vu <jvu246@aucklanduni.ac.nz>
2026-05-02 12:58:29 +1200

    Edited Ball.vhd to look better (follows a common syntax style we will be using for the whole project)

--- ball.vhd
+++ ball.vhd
@@ -4,38 +4,35 @@ use IEEE.std_logic_arith.all;
 use IEEE.std_logic_unsigned.all;
 
 
-entity Ball IS
-	PORT
-		( clk 						: IN std_logic;
-		  pixel_row, pixel_column	: IN std_logic_vector(9 DOWNTO 0);
-		  red, green, blue 			: OUT std_logic);		
-END Ball;
+entity Ball is
+	port (clk 							: in std_logic;
+			pixel_row, pixel_column	: in std_logic_vector(9 downto 0);
+			red, green, blue 			: out std_logic);		
+end entity Ball;
 
-architecture behavior of ball is
-
-SIGNAL ball_on					: std_logic;
-SIGNAL size 					: std_logic_vector(9 DOWNTO 0);  
-SIGNAL ball_y_pos, ball_x_pos	: std_logic_vector(9 DOWNTO 0);
-
-BEGIN           
-
-size <= CONV_STD_LOGIC_VECTOR(8,10);
--- ball_x_pos and ball_y_pos show the (x,y) for the centre of ball
-ball_x_pos <= CONV_STD_LOGIC_VECTOR(590,10);
-ball_y_pos <= CONV_STD_LOGIC_VECTOR(350,10);
+architecture ball_behavior of ball is
+	signal ball_on									 : std_logic;
+	signal size 									 : std_logic_vector(9 downto 0);  
+	signal ball_y_position, ball_x_position : std_logic_vector(9 downto 0);
+begin           
+	size <= conv_std_logic_vector(8,10);
+	
+	-- ball_x_position and ball_y_position show the (x,y) for the centre of ball
+	ball_x_position <= conv_std_logic_vector(590,10);
+	ball_y_position <= conv_std_logic_vector(350,10);
 
 
-ball_on <= '1' when ( ('0' & ball_x_pos <= pixel_column + size) and ('0' & pixel_column <= ball_x_pos + size) 	-- x_pos - size <= pixel_column <= x_pos + size
-					and ('0' & ball_y_pos <= pixel_row + size) and ('0' & pixel_row <= ball_y_pos + size) )  else	-- y_pos - size <= pixel_row <= y_pos + size
-			'0';
+	ball_on <= '1' when (('0' & ball_x_position <= pixel_column + size) 
+						and ('0' & pixel_column <= ball_x_position + size) -- x_pos - size <= pixel_column <= x_pos + size
+						and ('0' & ball_y_position <= pixel_row + size) 
+						and ('0' & pixel_row <= ball_y_position + size)) -- y_pos - size <= pixel_row <= y_pos + size
+						else '0';
 
-
--- Colours for pixel data on video signal
--- Keeping background white and square in red
-Red <=  '1';
--- Turn off Green and Blue when displaying square
-Green <= not ball_on;
-Blue <=  not ball_on;
-
-END behavior;
-
+	-- Colours for pixel data on video signal
+	-- Keeping background white and square in red
+	red <= '1';
+	
+	-- Turn off Green and Blue when displaying square
+	green <= not ball_on;
+	blue <= not ball_on;
+end architecture ball_behavior;
