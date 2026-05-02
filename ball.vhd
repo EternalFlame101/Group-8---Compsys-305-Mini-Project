2938cd1e51e5ca3c3ed0da95c9c57e51ed8d93c4
Johnny Vu <jvu246@aucklanduni.ac.nz>
2026-05-02 13:42:13 +1200

    Edited the Bouncy_Ball file to follow our teams coding conventions

--- ball.vhd
+++ ball.vhd
@@ -3,9 +3,8 @@ use IEEE.std_logic_1164.all;
 use IEEE.std_logic_arith.all;
 use IEEE.std_logic_unsigned.all;
 
-
 entity Ball is
-	port (clk 							: in std_logic;
+	port (clock, enable_pulse 		: in std_logic;
 			pixel_row, pixel_column	: in std_logic_vector(9 downto 0);
 			red, green, blue 			: out std_logic);		
 end entity Ball;
@@ -23,16 +22,16 @@ begin
 
 
 	ball_on <= '1' when (('0' & ball_x_position <= pixel_column + size) 
-						and ('0' & pixel_column <= ball_x_position + size) -- x_pos - size <= pixel_column <= x_pos + size
+						and ('0' & pixel_column <= ball_x_position + size) -- x_position - size <= pixel_column <= x_position + size
 						and ('0' & ball_y_position <= pixel_row + size) 
-						and ('0' & pixel_row <= ball_y_position + size)) -- y_pos - size <= pixel_row <= y_pos + size
+						and ('0' & pixel_row <= ball_y_position + size)) -- y_position - size <= pixel_row <= y_position + size
 						else '0';
 
 	-- Colours for pixel data on video signal
 	-- Keeping background white and square in red
-	red <= '1';
+	red 	<= '1';
 	
 	-- Turn off Green and Blue when displaying square
 	green <= not ball_on;
-	blue <= not ball_on;
+	blue 	<= not ball_on;
 end architecture ball_behavior;
--- bouncy_ball.vhd
+++ bouncy_ball.vhd
@@ -1,56 +1,53 @@
-LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.all;
-USE  IEEE.STD_LOGIC_ARITH.all;
-USE  IEEE.STD_LOGIC_SIGNED.all;
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_arith.all;
+use IEEE.std_logic_unsigned.all;
+
+entity Bouncy_Ball is
+	port (clock, enable_pulse, vertical_sync : in std_logic;
+			push_button_1, push_button_2 	 	  : in std_logic;
+         pixel_row, pixel_column	 		     : in std_logic_vector(9 downto 0);
+			red, green, blue 			 		     : out std_logic);		
+end entity Bouncy_Ball;
+
+architecture bouncy_ball_behavior of bouncy_ball is
+	signal ball_on				: std_logic;
+	signal size 				: std_logic_vector(9 downto 0);  
+	signal ball_y_position	: std_logic_vector(9 downto 0);
+	signal ball_x_position	: std_logic_vector(10 downto 0);
+	signal ball_y_motion		: std_logic_vector(9 downto 0);
+begin           
+	size <= conv_std_logic_vector(8,10);
+	
+	-- ball_x_position and ball_y_position show the (x,y) for the centre of ball
+	ball_x_position <= conv_std_logic_vector(590,11);
+
+	ball_on <= '1' when (('0' & ball_x_position <= '0' & pixel_column + size) 
+						and ('0' & pixel_column <= '0' & ball_x_position + size) -- x_position - size <= pixel_column <= x_position + size
+						and ('0' & ball_y_position <= pixel_row + size) 
+						and ('0' & pixel_row <= ball_y_position + size)) -- y_position - size <= pixel_row <= y_position + size
+						else '0';
 
 
-ENTITY bouncy_ball IS
-	PORT
-		( pb1, pb2, clk, vert_sync	: IN std_logic;
-          pixel_row, pixel_column	: IN std_logic_vector(9 DOWNTO 0);
-		  red, green, blue 			: OUT std_logic);		
-END bouncy_ball;
-
-architecture behavior of bouncy_ball is
-
-SIGNAL ball_on					: std_logic;
-SIGNAL size 					: std_logic_vector(9 DOWNTO 0);  
-SIGNAL ball_y_pos				: std_logic_vector(9 DOWNTO 0);
-SiGNAL ball_x_pos				: std_logic_vector(10 DOWNTO 0);
-SIGNAL ball_y_motion			: std_logic_vector(9 DOWNTO 0);
-
-BEGIN           
-
-size <= CONV_STD_LOGIC_VECTOR(8,10);
--- ball_x_pos and ball_y_pos show the (x,y) for the centre of ball
-ball_x_pos <= CONV_STD_LOGIC_VECTOR(590,11);
-
-ball_on <= '1' when ( ('0' & ball_x_pos <= '0' & pixel_column + size) and ('0' & pixel_column <= '0' & ball_x_pos + size) 	-- x_pos - size <= pixel_column <= x_pos + size
-					and ('0' & ball_y_pos <= pixel_row + size) and ('0' & pixel_row <= ball_y_pos + size) )  else	-- y_pos - size <= pixel_row <= y_pos + size
-			'0';
+	-- Colours for pixel data on video signal
+	-- Changing the background and ball colour by push buttons
+	Red 	<= push_button_1;
+	Green <= (not push_button_2) and (not ball_on);
+	Blue 	<= not ball_on;
 
 
--- Colours for pixel data on video signal
--- Changing the background and ball colour by pushbuttons
-Red <=  pb1;
-Green <= (not pb2) and (not ball_on);
-Blue <=  not ball_on;
-
-
-Move_Ball: process (vert_sync)  	
-begin
-	-- Move ball once every vertical sync
-	if (rising_edge(vert_sync)) then			
-		-- Bounce off top or bottom of the screen
-		if ( ('0' & ball_y_pos >= CONV_STD_LOGIC_VECTOR(479,10) - size) ) then
-			ball_y_motion <= - CONV_STD_LOGIC_VECTOR(2,10);
-		elsif (ball_y_pos <= size) then 
-			ball_y_motion <= CONV_STD_LOGIC_VECTOR(2,10);
+	Move_Ball: process (vertical_sync)  	
+	begin
+		-- Move ball once every vertical sync
+		if (rising_edge(vertical_sync)) then			
+			-- Bounce off top or bottom of the screen
+			if (('0' & ball_y_position >= conv_std_logic_vector(479,10) - size)) then
+				ball_y_motion <= - conv_std_logic_vector(2,10);
+			elsif (ball_y_position <= size) then 
+				ball_y_motion <= conv_std_logic_vector(2,10);
+			end if;
+			-- Compute next ball Y position
+			ball_y_position <= ball_y_position + ball_y_motion;
 		end if;
-		-- Compute next ball Y position
-		ball_y_pos <= ball_y_pos + ball_y_motion;
-	end if;
-end process Move_Ball;
-
-END behavior;
-
+	end process Move_Ball;
+end architecture bouncy_ball_behavior;
